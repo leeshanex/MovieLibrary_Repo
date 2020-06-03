71d03c56aea40938ecf4a6b404aa1e3cfb1e401d
Postman Integration <integration@postman.com>
2020-06-03 07:05:37 -0700

    Postman updated MovieLibrary_API 1.0.0 Commit reference: MDFkNDNiNDgtZTEwMy00YmJlLTg4MDEtZGFmMmI3NjM4Y2Zh

--- Client/app.js
+++ Client/app.js
@@ -1,3 +1,19 @@
+(function($){
+    function processForm( e ){
+        var dict = {
+        	Title : this["title"].value,
+        	Director: this["director"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+
 {
   "openapi": "3.0.0",
   "info": {
